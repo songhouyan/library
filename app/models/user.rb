2510b360f4ed8a589b678bb365a8e7b3adc226bf
Josue Lopez <josuesal24@gmail.com>
2018-03-11 14:02:07 -0600

    login with username or email incomplete

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates_uniqueness_of :username
+  attr_accessor :login
+end
