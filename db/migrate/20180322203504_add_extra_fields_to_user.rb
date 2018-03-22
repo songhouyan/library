364d4ffd4179a224be8ea4fd269ea0853bcd4448
Vuneu <vuneu@outlook.com>
2018-03-22 14:40:18 -0600

    Add new field to User

---
+++ db/migrate/20180322203504_add_extra_fields_to_user.rb
@@ -0,0 +1,8 @@
+class AddExtraFieldsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :age, :integer
+    add_column :users, :gender, :string
+    add_column :users, :bio, :text
+  end
+end
