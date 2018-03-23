3cbc7e0bb0795a4a7c08abad634668fd8be55896
malvov <manuel.olivaresbonilla@gmail.com>
2018-03-23 03:21:37 -0600

    fixed flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def flash_class(type)
+        { success: 'alert-success',
+          error:   'alert-danger',
+          alert:   'alert-warning',
+          notice:  'alert-info'
+        }[type.to_sym] || type.to_s
+    end
 end
